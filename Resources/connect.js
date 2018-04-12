4271053be97652f33e5fe3321a41a2e2621cd2ce
jtgjohn <gayj@rpi.edu>
2018-04-12 16:55:44 -0400

    added json

--- Resources/connect.js
+++ Resources/connect.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 		 	dataType: "json",
 		 	success: function(responseData, status){
 		  	var output = "<ul>";  
-		 	$.each(responseData.items, function(i, item) {
+		 	$.each(responseData.Students, function(i, item) {
 	   		output += '<h2>' + item.name + '<h2>';
 	    	output += '<p> Course(s): ' + item.courses + '</p>';
 	    	output += ' <p> Year: ' + item.year + '</p>';
-	    	output += '<p>' item.description + '</p>';
-	    	output += '<a href="' + item.link + '"> Connect </a>';
+	    	output += '<p>' item.profile + '</p>';
+	    	output += '<a href="' + item.addConnection + '"> Connect </a>';
 	  	});
 	  	output += "</ul>";
 	  	$('.person').html(output);
---
+++ Resources/connection.json
@@ -0,0 +1,11 @@
+{
+    "Students": [
+        {
+            "name": "Jason Smith",
+            "courses": "Data Structures",
+            "year": "Freshman",
+            "profile": "Hi! I'm looking for help with the data structures homeworks because I find them quite challenging.",
+            "addConnection": "idpkfdsalfdsa"
+        }
+    ]
+}
