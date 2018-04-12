bf91b713ab82ac6ae4945cbfbde89de6d5cce528
Jonathan Koppelman <jonathan.koppelm@gmail.com>
2018-04-12 16:55:40 -0400

    pushing changes

--- Scripts/forum.js
+++ Scripts/forum.js
@@ -1,4 +1,22 @@
+$(document).ready(function() {
+	window.alert("here");
+	$.ajax({
+		 	type: "GET",
+		 	url: "forum.json",
+		 	dataType: "json",
+		 	success: function(responseData, status){
+		  	var output = "<ul>";  
+		 	$.each(responseData.items, function(i, item) {
+	   		output += '<li class="discussion">' + item.Subject + '</li>';
+	    	output += '<li class="internalDisc">' + item.Messege + '</li>';
+	    	output += '<li class="author">' "Posted by "+item+" "+item.date+'</li>';
+	  	});
+	  	output += "</ul>";
+	  	$('.discussion').append(output);
 
-function populate(jFile){
-	$("discussion").append()
+		}, error: function(msg) {
+	  				// there was a problem
+	  	alert("There was a problem: " + msg.status + " " + msg.statusText);
+		}
+	});
 }
