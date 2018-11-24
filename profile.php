4481d5dcd4b57bd7116b79b89daacc6c7ef75860
Jonathan Koppelman <jonathan.koppelm@gmail.com>
2018-11-24 13:53:02 -0500

    Percentile calculation for ranking system

--- profile.php
+++ profile.php
@@ -44,15 +44,109 @@
 
             //pulls the user id from the session and accesses user table 
             //to find if the user is a tutor or a student 
-            $userid = $_SESSION["userid"]; //gets user id from session
+			$userid = $_SESSION["userid"]; //gets user id from session
             $namequery = "SELECT first_names,last_name from users where userid='". $userid ."'";
             $nameCall = $db->query($namequery);
             $resCall = $nameCall->fetch_assoc();
 
             //if the user is a tutor, find all connections where the tutorid
             //matches the userid to output connections
-                echo "<h3>".$resCall["first_names"]." ".$resCall["last_name"]."</h3>";
-                echo "<p>Hello I am a new student trying to learn how to code</p>";
+            echo "<h3>".$resCall["first_names"]." ".$resCall["last_name"]."</h3>";
+			echo "<p>Hello I am a new student trying to learn how to code</p>";
+			
+
+			/*************************************** RANKING SYSTEM ***************************************/
+			
+			
+			
+			//						                Calculating Percentile
+			
+			//getting single users Score
+			$userid = $_SESSION["userid"]; //gets user id from session
+			$scorequery = "SELECT score from users where userid='". $userid ."'";
+			$scoreCall = $db->query($scorequery);
+			$score = $scoreCall->fetch_assoc();
+
+			//getting avg of all scores
+			$averageScoreQ= "SELECT AVG(score) from users";
+			$avgScore = $db->query($averageScoreQ);
+			$maybe = $avgScore->fetch_assoc();
+
+			//getting all scores
+			$allScoresQ="SELECT score FROM users";
+			$allScores = $db->query($allScoresQ);
+			$numScores = $allScoresCall->num_rows;
+
+			//Calculating Standard Deviation
+			$summation = 0;
+			for ($i = 0; i<$numScores; $i++){
+				$currScore = $allScores->fetch_assoc();
+				$summation += pow(($currScore - $maybe),2);
+			}
+			$standardDev = sqrt(((float)$summation / (float)$numScores));
+			
+			//finding Percentile
+			$percentile=0;
+			$zScore = ($score - $maybe)/$standardDev;
+			
+			//Using a Z score -> Percentile on Normal Curve Chart
+			if($zScore < -2.5){
+				$percentile = 0;
+			}else if($zScore < -2 and $zScore>= -2.5){
+				$percentile = 1;
+			}else if($zScore < -1.5 and $zScore>= -2){
+				$percentile = 2-($zScore+2);
+			}else if($zScore < -1 and $zScore>= -1.5){
+				$percentile = 6-($zScore+1.5);
+			}else if($zScore < -0.5 and $zScore>= -1){
+				$percentile = 15-($zScore+1);
+			}else if($zScore < 0 and  $zScore>=-0.5){
+				$percentile = 30-($zScore+0.5);
+			}else if($zScore > 0 and $zScore <= 0.5){
+				$percentile = 69-(0.5-$zScore);
+			}else if($zScore > 0.5 and $zScore <= 1){
+				$percentile = 84-(1-$zScore);
+			}else if($zScore >1 and $zScore <=1.5){
+				$percentile = 93-(1.5-$zScore);
+			}else if($zScore >1.5 and $zScore <=2){
+				$percentile = 97-(2-$zScore);
+			}else if($zScore >2 and $zScore<=2.5){
+				$percentile = 99-(1.5-$zScore);
+			}else if($zScore>2.5){
+				$percentile = 100-(3-$zScore);
+			}else{
+				//ERROR
+				$percentile=-7768;
+			}
+			
+			
+			if($percentile>100){
+				$percentile = 100;
+			}
+			if($percentile<0){
+				$percentile=0;
+			}
+		
+			//										Done with Percentile
+			
+			
+			
+			
+			// if($averageScore == 0){
+			// 	if($score['score']!=$averageScore){
+			// 		//rank above average
+			// 	}
+			// 	//rank at average
+			// }else{
+			// 	$percentile = ((double) $score['score'] / (double) $averageScore)*100;
+			// 	//getting the ranking of user
+			// 	switch($percentile){
+			// 		case()
+			// 	}
+			// }
+			
+
+			/*********************************** END OF RANKING SYSTEM ***********************************/
 
         ?>
         
