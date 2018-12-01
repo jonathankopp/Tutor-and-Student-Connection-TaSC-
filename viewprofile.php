bf4edb9aec093f9616bfc68a6772421b51bcfd9b
jtgjohn <gayj@rpi.edu>
2018-12-01 16:19:52 -0500

    other profile

---
+++ viewprofile.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+
+
+<html>
+<head>
+	<title>TaSC Connections</title>
+	<link href="Resources/connect.css" rel="stylesheet" type="text/css"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <script type="text/javascript" src="Resources/jquery-1.4.3.min.js"></script>
+</head>
+
+
+
+</html>
